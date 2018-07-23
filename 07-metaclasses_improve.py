8400ec4d21af58b290b2a8683bbdab43c592a202
scorpionipx <scorpionipx@gmail.com>
2018-07-23 14:21:20 +0300

    dataclasses

--- 07-metaclasses_improve.py
+++ 07-metaclasses_improve.py
@@ -1,5 +1,30 @@
 from math import pi
 
+def dataclass(cls):
+    def __init__(self, *args, **kwargs):
+        for k, v in zip(cls.__annotations__.keys(), args):
+            setattr(self, k, v)
+        for k, v in kwargs.items():
+            if k not in cls.__annotations__:
+                raise TypeError(
+                    f"__init__() got an unexpected keyword argument '{k}'"
+                )
+            setattr(self, k, v)
+
+        post_init = getattr(self, '__post_init__', None)
+        if callable(post_init):
+            post_init()
+
+    def __repr__(self):
+        kwargs = {k: getattr(self, k) for k in cls.__annotations__}
+        kwstr = ', '.join([f'{k}={v}' for k, v in kwargs.items()])
+        return f'{cls.__name__}({kwstr})'
+
+    setattr(cls, '__init__', __init__)
+    setattr(cls, '__repr__', __repr__)
+    setattr(cls, '__str__', __repr__)
+    return cls
+
 
 class TypeChecker:
     required_type = object
@@ -34,41 +59,21 @@ class TypeCheckMeta(type):
 class Base(metaclass=TypeCheckMeta):
     __annotations__ = {}
 
-
+@dataclass
 class Point(Base):
     x: int
     y: int
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
 
     def move_by(self, dx, dy):
         self.x += dx
         self.y += dy
 
-    def __str__(self):
-        return f'A Point at {self.x}, {self.y}'
-
-    def __repr__(self):
-        return f'{self.__class__.__name__}({self.x}, {self.y})'
-
-
+@dataclass
 class Circle(Base):
     center: Point
     radius: int
-
-    def __init__(self, center, radius):
-        self.center = center
-        self.radius = radius
 
     @property
     def area(self):
         return pi * self.radius ** 2
 
-    def __str__(self):
-        return f'A Circle at {self.center.x}, {self.center.y} and ' + \
-               f'radius {self.radius}'
-
-    def __repr__(self):
-        return f'{self.__class__.__name__}({self.center!r}, {self.radius!r})'
